340b1d72197b35528d3b0ed5f87ad17c1a45ca90
flaggton <markusvogel12@gmail.com>
2024-07-17 22:31:09 +0200

    impl. errorpage + routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { TestComponent } from './test/test/test.component';
+import { ErrorpageComponent } from './errorpage/errorpage/errorpage.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-    {path: 'dashboard', component: DashboardComponent, pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'test', component: TestComponent},
+    {path: '**', component: ErrorpageComponent},
 ];
