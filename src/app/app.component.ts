56e9745e2a3f9ac151a010dfdc8ee9eb8733fb23
flaggton <markusvogel12@gmail.com>
2024-07-17 22:59:31 +0200

    impl navigation.component + styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { NavigationComponent } from "./shared/navigation/navigation.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,DashboardComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, DashboardComponent, NavigationComponent]
 })
 export class AppComponent {
   title = 'ng-website';
