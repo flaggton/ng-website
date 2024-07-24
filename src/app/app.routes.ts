d9564360dcb39be8834ad2567d452b7ad8bae698
flaggton <markusvogel12@gmail.com>
2024-07-24 22:57:06 +0200

    impl dark-angels Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { TestComponent } from './test/test/test.component';
 import { ErrorpageComponent } from './errorpage/errorpage/errorpage.component';
 import { UltramarinesComponent } from './ultramarines/ultramarines/ultramarines.component';
+import { DarkAngelsComponent } from './dark-angels/dark-angels/dark-angels.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
     {path: 'dashboard', component: DashboardComponent},
-    {path: 'test', component: TestComponent},
     {path: 'ultramarines', component: UltramarinesComponent},
+    {path: 'dark-angels', component: DarkAngelsComponent},
+    {path: 'test', component: TestComponent},
     {path: '**', component: ErrorpageComponent},
 ];
---
+++ src/app/dark-angels/dark-angels/dark-angels.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dark-angels',
+  standalone: true,
+  imports: [],
+  templateUrl: './dark-angels.component.html',
+  styleUrl: './dark-angels.component.scss'
+})
+export class DarkAngelsComponent {
+
+}
